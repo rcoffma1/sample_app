4fe0083d57c98686ec6272d0619d7c5091577deb
rcoffma1 <ron.coffman@daveramsey.com>
2020-08-23 06:27:29 -0500

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  def make_assertions(title)
+    assert_response :success
+    assert_select "title", "#{title} | #{@base_title}"
+  end
+
+  test "should get root" do
+    get root_url 
+    make_assertions("Home")
+  end
+  
   test "should get home" do
     get static_pages_home_url
-    assert_response :success
+    make_assertions("Home")
   end
 
   test "should get help" do
     get static_pages_help_url
-    assert_response :success
+    make_assertions("Help")
   end
 
   test "should get about" do
     get static_pages_about_url
-    assert_response :success
+    make_assertions("About")
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    make_assertions("Contact")
   end
 
 end

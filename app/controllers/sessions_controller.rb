a8fc58d3124c71fdc92ac76b36b56c9a1ac0cfb6
rcoffma1 <ron.coffman@daveramsey.com>
2020-09-22 12:44:34 -0500

    implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create 
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      # rcoffma1@gmail.com/foodaddy
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
